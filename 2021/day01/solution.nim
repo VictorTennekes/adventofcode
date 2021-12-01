5b86a16c6de56b28dd2eab1a4ee42468ddaea00d
Victor Tennekes <victor@tennekes.nl>
2021-12-01 12:52:26 +0100

    Feat: added combined version

---
+++ 2021/day01/solution.nim
@@ -0,0 +1,19 @@
+import std/strutils
+import std/sequtils
+
+const
+ input = readFile("input.txt").splitLines().filterIt(it != "").mapIt(parseInt(it))
+ size = len(input)
+
+var
+ second_part = newSeq[int](0)
+for i in 0..<size-2:
+ second_part.add(input[i] + input[i+1] + input[i+2])
+
+proc solve(values: seq) : int =
+ for i in 1..<len(values):
+  if values[i] > values[i - 1]:
+   result += 1
+
+echo solve(input)
+echo solve(second_part)
