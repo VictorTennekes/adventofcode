775e0b3de7c127ad40db479b372e9bf0249acfa7
Victor Tennekes <victor@tennekes.nl>
2021-12-08 11:47:31 +0100

    Feat: finished and made a combined version

--- 2021/day08/part2.nim
+++ 2021/day08/part2.nim
@@ -1,51 +1,46 @@
 include prelude
 
 let
- input = readFile("test.txt").strip.split("\n")
- size = input.len
+ input = readFile("input.txt").strip.split("\n")
  codeChars = "abcdefg"
 
-func getValue(code: string): string =
+func strdiff(one: string, two: string): char =
+ result = ' '
+ for i in one:
+  if i notin two:
+   result = i
+
+func getValue(code: string, topr: char, botr: char, mid: char): string =
  case code.len:
   of 2: result = "1"
   of 3: result = "7"
   of 4: result = "4"
+  of 5:
+   if topr in code and botr in code: result = "3"
+   elif topr in code: result = "2"
+   else: result = "5"
+  of 6:
+   if mid notin code: result = "0"
+   elif topr in code: result = "9"
+   elif botr in code: result = "6"
   of 7: result = "8"
-  else:
-   return (code)
-
-func getdecodedValue(code: string, top: char, bot: char): string =
- result = code
- if code.len == 5:
-  if top in code and bot in code:
-   result = "3"
-  elif top in code:
-   result = "5"
-  else:
-   result = "2"
- elif code.len == 6:
-  if top in code and bot in code:
-   result = "9"
-  else:
-   result = "6"
-
-var res = 0
+  else: result = "0"
 
 var codes: seq[string]
-for line in input:
- var one: string
- var topr, botr, topl, botl: char
- codes = line.split(" ").filterIt(it != "|")
- for i in 10..codes.high:
-  codes[i] = getValue(codes[i])
- for i in codes:
-  if i.len == 2:
-   one = i
- 
+var res = 0
 
- for i in 10..codes.high:
-  codes[i] = getdecodedValue(codes[i], topr, botr)
- echo codes
- var local = codes[10..codes.high].join
- res.inc(parseInt(local))
+for i in input:
+ codes = i.split(" ").filterIt(it != "|")
+ let one = codes.filterIt(it.len == 2)[0]
+ var botr, topr, topl, mid: char
+ for i in codes[0..9]:
+  if i.len == 6 and strdiff(one, i) != ' ': topr = strdiff(codeChars, i)
+ botr = strdiff(one, $topr)
+ for i in codes[0..9]:
+  if i.len == 5 and getValue(i, topr, botr, ' ') == "2":
+   topl = strdiff(codeChars, i & one)
+ for i in codes[0..9]:
+  if i.len == 4:
+   mid = strdiff(i, $botr & $topr & $topl)
+ res.inc(parseInt(codes[10..codes.high].mapIt(getValue(it, topr, botr, mid)).join))
 echo res
---
+++ 2021/day08/solve.nim
@@ -0,0 +1,54 @@
+include prelude
+
+let
+ input = readFile("input.txt").strip.split("\n")
+ size = input.len
+
+func strdiff(s1: string, s2: string): string =
+ result = " "
+ for i in s1:
+  if i notin s2:
+   result = $i
+
+func getValue(code: string, topr, botr, mid: string): string =
+ result = case code.len:
+  of 2: "1"
+  of 3: "7"
+  of 4: "4"
+  of 5:
+   if topr in code and botr in code: "3"
+   elif topr in code: "2"
+   else: "5"
+  of 6:
+   if mid notin code: "0"
+   elif topr in code: "9"
+   else: "6"
+  of 7: "8"
+  else: "0"
+
+func part1(input: seq[string]): int =
+ for line in input:
+  result.inc(line.split("|")[1].split(" ").filterIt(it.len in [2,3,4,7]).len)
+
+func part2(input: seq[string]): int =
+ var codes: seq[string]
+ let codeChars = "abcdefg"
+
+ for i in input:
+  codes = i.split(" ").filterIt(it != "|")
+  let one = codes.filterIt(it.len == 2)[0]
+  var botr, topr, topl, mid: string
+  for i in codes[0..9]:
+   if i.len == 6 and strdiff(one, i) != " ": topr = strdiff(codeChars, i)
+  botr = strdiff(one, topr)
+  for i in codes[0..9]:
+   if i.len == 5 and getValue(i, topr, botr,"") == "2":
+    topl = strdiff(codeChars, i & one)
+  for i in codes[0..9]:
+   if i.len == 4:
+    mid = strdiff(i, botr & topr & topl)
+  result.inc(parseInt(codes[10..codes.high].mapIt(getValue(it, topr, botr, mid)).join))
+
+
+echo part1(input)
+echo part2(input)
