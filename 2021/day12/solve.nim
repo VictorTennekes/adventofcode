08a04b90b6e786f7614d31dd573a252554c0f831
Victor Tennekes <victor@tennekes.nl>
2021-12-13 11:45:04 +0100

    Fix: sped up day12

--- 2021/day12/solve.nim
+++ 2021/day12/solve.nim
@@ -2,36 +2,26 @@ include ../aoc
 
 let input = readFile("input.txt").strip.split("\n")
 
-func getOptions(input: seq[string]): Table[string, HashSet[string]] =
+func getOptions(input: seq[string]): Table[string, seq[string]] =
  for line in input:
-  let (ok, cave1, cave2) = scanTuple(line, "$*-$*")
-  if ok:
-   if not result.hasKey(cave1):
-    result[cave1] = initHashSet[string]()
-   if not result.hasKey(cave2):
-    result[cave2] = initHashSet[string]()
-   result[cave1].incl(cave2)
-   result[cave2].incl(cave1)
+  let (_,x,y) = scanTuple(line, "$*-$*")
+  result.mgetOrPut(x, @[]).add(y)
+  result.mgetOrPut(y, @[]).add(x)
 
 let options = getOptions(input)
 
-proc Search(curr: string, visited: HashSet[string], dup: string): int =
- var localVisited = visited
- var dup = dup
- if curr == "end":
-  return 1
- if dup != "off":
-  if curr == "start" and visited.len > 0:
-   return 0
- if curr.all(isLowerAscii) and curr in visited:
-  if dup == "on":
-   dup = curr
-  else:
-   return(0)
- localVisited.incl curr
- for next in options[curr]:
-  result += Search(next, localVisited, dup)
+proc genPaths(paths: var int, visited: seq[string], dup:int) =
+ if visited[^1] == "end":
+  inc paths
+ else:
+  for cave in options[visited[^1]]:
+   if cave[0].isUpperAscii or cave notin visited:
+    genPaths(paths, visited & cave, dup)
+   elif dup > 0 and cave != "start":
+    genPaths(paths, visited & cave, dup - 1)
+    
+proc getPaths(dup=0): int =
+ genPaths(result, @["start"], dup)
 
-let visited = initHashSet[string]()
-echo Search("start", visited, "off")
-echo Search("start", visited, "on")
+echo getPaths()
+echo getPaths(1)
