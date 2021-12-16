fea9eae2ad11828d0054f1b1682682c86625f727
Victor Tennekes <victor@tennekes.nl>
2021-12-16 11:32:25 +0100

    Feat: Day16

--- 2021/day16/part1.nim
+++
@@ -1,6 +0,0 @@
-include ../aoc
-
-let
- input = readFile("test.txt").strip.mapIt(fromHex[int]($it).toBin(4)).join
-
-echo input
---
+++ 2021/day16/solve.nim
@@ -0,0 +1,60 @@
+include ../aoc
+
+var data = readFile("input.txt").strip.mapIt(parseHexInt($it).toBin(4)).join
+
+type
+ Packet = object
+  version: int
+  typeId: int
+  value: int
+  subPacks: seq[Packet]
+
+proc takeStr(bits: var string, len: int): string =
+ result = bits[0..<len]
+ bits.delete(0..<len)
+
+proc takeNum(bits: var string, len: int): int =
+ result = parseBinInt(takeStr(bits, len))
+
+proc parsePacket(bits: var string): Packet =
+ result.version = bits.takeNum(3)
+ result.typeId = bits.takeNum(3)
+ if result.typeId == 4:
+  var value = ""
+  while true:
+   let bit = bits.takeStr(5)
+   value &= bit[1..^1]
+   if bit[0] == '0':
+    break
+  result.value = value.parseBinInt
+ else:
+  let lenType = bits.takeNum(1)
+  if lenType == 0:
+   let bitLength = bits.takeNum(15)
+   let targetLen = bits.len - bitLength
+   while bits.len > targetLen:
+    result.subPacks.add bits.parsePacket
+  else:
+   let packetCount = bits.takeNum(11)
+   while result.subPacks.len < packetCount:
+    result.subPacks.add bits.parsePacket
+
+var p1 = 0
+proc eval(packet: Packet): int =
+ p1.inc packet.version
+ let subPacks = packet.subPacks.map(eval)
+ result = case packet.typeId:
+  of 0: subPacks.sum
+  of 1: subPacks.prod
+  of 2: subPacks.min
+  of 3: subPacks.max
+  of 4: packet.value
+  of 5: int(subPacks[0] > subPacks[1])
+  of 6: int(subPacks[0] < subPacks[1])
+  else:
+   int(subPacks[0] == subPacks[1])
+
+let parentPack = data.parsePacket
+let p2 = eval(parentPack)
+echo "part 2: ", p1
+echo "part 1: ", p2
