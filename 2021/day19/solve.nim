132c2b144fe6bb18a10a49c0bf6432da3f24750a
Victor Tennekes <vtenneke@student.codam.nl>
2021-12-20 00:23:33 +0000

    Fix: works, quite slow though

--- 2021/day19/solve.nim
+++ 2021/day19/solve.nim
@@ -2,50 +2,31 @@ include ../aoc
 
 type
  Point3 = tuple
-  x: int
-  y: int
-  z: int
+  x, y, z: int
 
 template `$`*(a:Point3):string = ($a.x & "," & $a.y & "," & $a.z)
 template `+`*(a,b:Point3):Point3 = (a.x+b.x, a.y+b.y, a.z+b.z)
 template `-`*(a,b:Point3):Point3 = (a.x-b.x, a.y-b.y, a.z-b.z)
 template `-`*(a:Point3):Point3 = (-a.x, -a.y, -a.z)
 
-func rotations(p: Point3, perm: int): Point3 =
- case perm:
-  of 1: return (p.x, -p.y, -p.z)
-  of 2: return (-p.x, p.y, -p.z)
-  of 3: return (-p.x, -p.y, p.z)
-  of 4: return (p.x, p.z, -p.y)
-  of 5: return (p.x, -p.z, p.y)
-  of 6: return (-p.x, p.z, p.y)
-  of 7: return (-p.x, -p.z, -p.y)
-
-  of 8: return (p.y, p.z, p.x)
-  of 9: return (p.y, -p.z, -p.x)
-  of 10: return (-p.y, p.z, -p.x)
-  of 11: return (-p.y, -p.z, p.x)
-  of 12: return (p.y, p.x, -p.z)
-  of 13: return (p.y, -p.x, p.z)
-  of 14: return (-p.y, p.x, p.z)
-  of 15: return (-p.y, -p.x, -p.z)
-
-  of 16: return (p.z, p.x, p.y)
-  of 17: return (p.z, -p.x, -p.y)
-  of 18: return (-p.z, p.x, -p.y)
-  of 19: return (-p.z, -p.x, p.y)
-  of 20: return (p.z, p.y, -p.x)
-  of 21: return (p.z, -p.y, p.x)
-  of 22: return (-p.z, p.y, p.x)
-  of 23: return (-p.z, -p.y, -p.x)
-  else: return (p.x, p.y, p.z)
+proc rotations(p: Point3, n: int): Point3 =
+ let (x, y, z) = (p.x, p.y, p.z)
+ let trs = [(x, z, -y), (-z, x, -y), (-x, -z, -y),
+            (z, -x, -y), (z, -y, x), (y, z, x),
+            (-z, y, x), (-y, -z, x), (-y, x, z),
+            (-x, -y, z), (y, -x, z), (x, y, z),
+            (-z, -x, y), (x, -z, y), (z, x, y),
+            (-x, z, y), (-x, y, -z), (-y, -x, -z),
+            (x, -y, -z), (y, x, -z), (y, -z, -x),
+            (z, y, -x), (-y, z, -x), (-z, -y, -x)]
+ return trs[n]
 
 func permuteScanner(scanner: seq[Point3], index: int): seq[Point3] =
  for beacon in scanner:
   let rs = beacon.rotations(index)
   result.add rs
 
-let input = readFile("input.txt").strip.split("\n\n")
+let input = readFile("input.txt").split("\n\n")
 var scanners: seq[seq[(Point3)]]
 for part in input:
  var scanner: seq[Point3]
@@ -54,33 +35,36 @@ for part in input:
   scanner.add (x, y, z)
  scanners.add scanner
 
-func checkOverlap(a, b: seq[Point3]): (bool, Point3) =
- var distances: CountTable[Point3]
- for ba in a:
-  for bb in b:
-   distances.inc(ba-bb, 1)
- let great = distances.largest
- if great.val >= 12:
-  return (true, great.key)
+func checkOverlap(scan: seq[seq[Point3]], rs: seq[Point3], solved: HashSet[int]): (bool, Point3) =
+ for index in solved:
+  var distances: CountTable[Point3]
+  for ba in scan[index]:
+   for rb in rs:
+    let tb = ba - rb
+    distances.inc(tb, 1)
+    if distances[tb] >= 12:
+     return (true, tb)
  return (false, (0,0,0))
 
-var normalScanners: HashSet[Point3]
+var solved = [0].toHashSet
 var points: HashSet[Point3]
-let anchor = scanners[0]
-scanners.delete(0)
-normalScanners.incl anchor.toHashSet
-while scanners.len > 0:
- var tmpScanners = scanners
- for index, scanner in tmpScanners:
-  block permutation:
-   for i in 0..23:
-    var rs = permuteScanner(scanner, i)
-    let (valid, translation) = checkOverlap(normalScanners.toSeq, rs)
-    if (valid):
-      points.incl translation
-      rs = rs.map(a => a + translation)
-      normalScanners.incl rs.toHashSet
-      scanners.delete(index)
-      break permutation
+while solved.len < scanners.len:
+ for index, scanner in scanners:
+  if index in solved: continue
+  for i in 0..23:
+   var rs = permuteScanner(scanner, i)
+   let (valid, translation) = checkOverlap(scanners, rs, solved)
+   if valid:
+     points.incl translation
+     scanners[index] = rs.map(a => a + translation)
+     solved.incl index
+     break
 
-echo normalScanners.len
+echo scanners.foldl(a & b).toHashSet.len
+
+var largest = 0
+for i in points:
+ for j in points:
+  let d = i - j
+  largest = largest.max(abs(d.x)+abs(d.y)+abs(d.z))
+echo largest
