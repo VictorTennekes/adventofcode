50970c24ffc7dfc5014c5431ae2e02369b503ca7
Victor Tennekes <vtenneke@student.codam.nl>
2021-12-21 11:17:17 +0000

    Fix: day21 nicer

--- 2021/day21/solve.nim
+++ 2021/day21/solve.nim
@@ -4,30 +4,30 @@ import memo
 let input = readFile("input.txt").split("\n").mapIt(it.split(": "))
 
 proc part1(): int =
- var positions = [input[0][1].parseInt, input[1][1].parseInt]
+ var pos = [input[0][1].parseInt, input[1][1].parseInt]
  var scores = [0, 0]
- var turn, countRolls = 0
+ var turn, rolls = 0
  proc roll(rep=1):int =
   for _ in 1..rep:
-   inc countRolls
-   result += (countRolls - 1) mod 100 + 1
+   inc rolls
+   result += (rolls - 1) mod 100 + 1
  while true:
-  positions[turn] = (positions[turn] + roll(3) - 1) mod 10 + 1
-  scores[turn] += positions[turn]
+  pos[turn] = (pos[turn] + roll(3) - 1) mod 10 + 1
+  scores[turn] += pos[turn]
   if scores[turn] >= 1000:
-   return scores[1-turn] * countRolls
+   return scores[1-turn] * rolls
   turn = 1 - turn
 
 proc part2(): int =
  const rolls = {3: 1, 4: 3, 5: 6, 6: 7, 7: 6, 8: 3, 9: 1}
  func countWins(p1, p2, s1, s2: int): array[2, int] {.memoized.} =
   if s1 >= 21 or s2 >= 21:
-   return [int(s1>=21), int(s2>=21)]
-  for (roll,freq) in rolls:
+   return [int(s1 >= 21), int(s2 >= 21)]
+  for (roll, freq) in rolls:
    let p = (p1 + roll - 1) mod 10 + 1
    let w = countWins(p2, p, s2, s1 + p)
-   result[0] += freq*w[1]
-   result[1] += freq*w[0]
+   result[0] += freq * w[1]
+   result[1] += freq * w[0]
  max countWins(input[0][1].parseInt, input[1][1].parseInt, 0, 0)
 
 echo "part 1: ", part1()
